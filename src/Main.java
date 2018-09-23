5a6e2f1e65c098ddee8780312bd35505909f239c
Ostap Strashevskii <strashevskyseo@gmail.com>
2018-09-23 14:27:26 +0300

    commit4author

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("upgrade");
         System.out.println("upgrade2");
+        System.out.println("upgrade3");
     }
     
 }
